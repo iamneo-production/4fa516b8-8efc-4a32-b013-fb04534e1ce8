40d0037c3bee7c15800c46f899b55e1c1701237c
rasikaprakash <rasikaprakash109@gmail.com>
2023-07-14 07:09:46 +0000

    Code commited

---
+++ reactapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+// import Foodroute from './Foodroute/Foodroute'
+import FoodOrderingPortal from './Foodorderingporatl';
+import FoodItem from './Fooditem';
+import FoodPage from './Fooditem';
+
+function App() {
+  return (
+   <FoodPage />
+  );
+}
+
+export default App;
